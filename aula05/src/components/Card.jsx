8c972e7adfcc974fa37153f48af7593707f23753
andersonsilva8609 <andersonsilva8609@gmail.com>
2023-03-16 14:19:15 -0300

    Revert "update arquivos materia react"
    This reverts commit 145b0a970b44b50d34c16f245a0b7832b4982a82.

--- aula05/src/components/Card.jsx
+++
@@ -1,9 +0,0 @@
-export default function Card(props){
-    return(
-        <div>
-            <h4>{props.dado.titulo}</h4>
-            <p>{props.dado.subtitulo}</p>
-            <img src={props.dado.url}  />
-        </div>
-    );
-}
